3b1fb50a416bbc72cd122f685e4873d28248fc58
404 <daehyeon7902@gmail.com>
2024-09-27 12:24:13 +0900

    ver0.4

--- week04/alpha.go
+++ week04/alpha.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+	i := 55
+	var f float32 = 4.30
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i))
+	fmt.Printf("%s\n", strings.Title("inhatc"))
+	fmt.Println(math.Ceil(3.99))
+	fmt.Printf("value i : %d\n", i)
+	fmt.Print("value i : ", i, "\n")
+	fmt.Println("value i :", i)
 }
